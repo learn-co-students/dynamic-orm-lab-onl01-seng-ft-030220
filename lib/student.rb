c0b62d80637a0e498c27d87e38f27e4d92bd6536
Brandon Dusch <brandondusch@gmail.com>
2020-04-07 01:10:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,25 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name 
-    self.to_s.downcase.pluralize
-  end
-  
-  def self.column.names 
-    # Makes it so we can access returned SQL queries as hashes
-    DB[:conn].results_as_hash = true
-    
-    
-    sql = "PRAGMA table_info('#{table_name}')"
-    
-    table_info = DB[:conn].execute(sql)
-    
-    column_names = []
-    
-    table_info.each do |column|
-      column_names << column["name"]
-    end
-    
-    column_names
+  self.column_names.each do |column_name|
+    attr_accessor column_name.to_sym
   end
 end
