4637e4a7b72bf319cd5d30ff6f2b2e11cdb6b56a
Ocelots-etc <catherinedgomez@gmail.com>
2020-04-16 21:35:35 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require_relative '../spec/spec_helper'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    
+    self.column_names.each do |attr|
+        attr_accessor attr.to_sym
+    end
 end
